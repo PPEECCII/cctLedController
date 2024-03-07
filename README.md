c49f9473c8978a3ecb8674de525edcc8023edb73
Pastor Camargo <pastoresteban.camargo.contractor@bbva.com>
2024-03-07 12:59:52 -0600

    Initial version commit Added the program first version code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cctLedController
 program to control an CCT(warm - cold) led strip
+This code is used to control a CCT led strip using one of the capacitive touch pins of the esp32 device.
+short pushes CYCLES between the modes: OFF - WARM LIGHT - WHITE LIGHT - COLD LIGHT - OFF
+long presses alternate between increasing and decreasing light intensity
+
+TOUCH_PIN must be one of the touch capable esp32 pins (TOUCH0-TOUCH9)
+COLD_LIGHT must be an output safe esp32 pin
+WARM_LIGHT must be an output safe esp32 pin
+
+In order to wire COLD_LIGHT AND WARM_LIGHT you will need a NPN transistor and a 1k resistor for each pin, and must be connected this way:
+  -Resistor connected to esp32 pin in one side and transistor base in the other side
+  -Transistor emitter to ground
+  -Transistor collector to the led strip W(warm) or C(cold) pin
+
+The led power supply ground must be connected to the esp32 ground.
+If the led strip is powered with more than 5v you can use the same power supply for the esp32 using a step-down converter.
---
+++ cctLedController.ino
@@ -0,0 +1,84 @@
+#include <math.h>
+#define TOUCHED_MAX_VALUE 70 //Capacitive touch max touched value
+
+#define TOUCH_PIN 4 //GPIO touch pin number
+
+#define COLD_LIGHT 32 //Cold light control pin
+#define WARM_LIGHT 33 //Warm light control pin
+
+#define TOUCH_DURATION 300 //Looping delay time
+
+#define RESOLUTION 4 //PWM frequency resolution
+
+#define LONG_TOUCH_DURATION 3 //Number of touch duration that are considered a long press
+
+#define STATUS_OFF 0 //Off Status constant
+#define STATUS_WARM 1 //Warm light status constant
+#define STATUS_WHITE 2 //White light status constant
+#define STATUS_COLD 3 //Cold light status constant
+
+int count = 0;
+
+int currentValue=1;
+
+boolean isIncreasing = true;
+
+int maxValue;
+
+int status = STATUS_OFF;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+  Serial.println("ESP32 Touch Test");
+
+  maxValue = pow(2.0, RESOLUTION) - 1;
+  analogWriteResolution(RESOLUTION);
+  setLight();
+}
+
+void loop() {
+  boolean touched = touchRead(TOUCH_PIN) < TOUCHED_MAX_VALUE;
+  if(touched) {
+    if(++count >= LONG_TOUCH_DURATION) handleLongPress(count - LONG_TOUCH_DURATION);
+  } else {
+    if(count > 0 && count < LONG_TOUCH_DURATION) handlePush();
+    count = 0;
+  }
+  delay(TOUCH_DURATION);
+}
+
+void handlePush() {
+    Serial.println("CORTO");
+    if(status == STATUS_OFF) {
+      status = STATUS_WARM;
+    } else if(status == STATUS_WARM) {
+      status = STATUS_WHITE;
+    } else if(status == STATUS_WHITE) {
+      status = STATUS_COLD;
+    } else status = STATUS_OFF;
+    setLight();
+}
+
+void handleLongPress(int counter) {
+    Serial.print("LARGO ");
+    Serial.println(counter);
+    if(counter <= 0) { 
+      isIncreasing = !isIncreasing;
+    }
+    if(isIncreasing) {
+      if(currentValue < maxValue) 
+        currentValue++;
+    }
+    else if(currentValue > 1) 
+      currentValue--;
+    
+    setLight();
+}
+
+void setLight() {
+  Serial.println(status);
+  Serial.println(currentValue);
+  analogWrite(WARM_LIGHT, (status == STATUS_WARM || status == STATUS_WHITE) ? currentValue : 0);
+  analogWrite(COLD_LIGHT, (status == STATUS_COLD || status == STATUS_WHITE) ? currentValue : 0);
+}
